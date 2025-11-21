e960ccbc537849e36a6598e305c62d110e9521e6
skendzierski@st.swps.edu.pl <skendzierski@st.swps.edu.pl>
2025-11-21 13:31:51 +0100

    Lab7

---
+++ uzu_lab4/biblioteka/urls.py
@@ -0,0 +1,10 @@
+# plik biblioteka/urls.py
+
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('books/', views.book_list),
+    path('books/<int:pk>/', views.book_detail),
+]
+
